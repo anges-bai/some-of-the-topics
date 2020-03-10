e71989f294d28d2cc2cb588df3c31db2ef3b69fc
anges-bai <45257069+anges-bai@users.noreply.github.com>
2020-03-10 18:11:28 +0800

    Create primeNumber.java

---
+++ primeNumber.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ *  题目：判断101-200之间有多少个素数，并输出所有素数。
+ *
+ *  分析：什么是素数，不能被自己和1整除的数。
+ */
+public class Subject2 {
+    public static void main(String[] args) {
+        getPrimeNumber(101,200);
+    }
+
+    /**
+     * 判断a和b之间有多少个素数
+     * @param a
+     * @param b
+     */
+    public static void getPrimeNumber(int a,int b){
+        int sum = 0;
+        if(a >= b){
+            System.out.println("a不能大于等于b！");
+        }
+        System.out.print("素数：");
+        for(int i=a;i<=b;i++){
+            if(isPrimeNumber(i)){
+                sum += 1;
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println();
+        System.out.println("存在素数数量："+sum);
+    }
+
+    /**
+     * 判断num是否是一个素数
+     * @param i
+     * @return
+     */
+    private static boolean isPrimeNumber(int i) {
+        boolean flag = true;
+        for(int j=2;j <= i/2;j++){
+            if(i%j == 0){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+}
+
